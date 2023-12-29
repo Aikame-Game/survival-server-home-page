634e565cfa2f8b6a0ad16791035cfad2316ad37e
Aikame-Game <148684237+Aikame-Game@users.noreply.github.com>
2023-12-29 09:10:26 +0900

    function copy() {.js

--- function copy() {.js
+++ function copy() {.js
@@ -1,6 +1,9 @@
-function copy() {
-    var targetCode = document.getElementById("kamesurvival.aternos.me:24344"); // コピー対象のテキストを選択する
-    targetCode.select();                              // コピーしたい要素を選択状態にする
-    document.execCommand("Copy");                     // 選択しているテキストをクリップボードにコピーする
-    alert("コピーしました！");                            // 任意でアラートを出す
-}
+var cpytxt = $('.cpytext, .wp-block-code');
+cpytxt.append('<button class="cpybtn"><i class="far fa-copy"></i></button>'); 
+var btn = $('.cpybtn');
+btn.on('click', function() {
+  var cpytxt = $(this).parent().text();
+  navigator.clipboard.writeText(cpytxt);
+  $(this).parent().prepend('<span class="balloon_top">コピーしました！</span>');
+  setTimeout(() => $(this).parent().find('span.balloon_top').remove(), 1000);
+});  
